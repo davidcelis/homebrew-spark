736197c816a6acac45525c6693c6abfe8fc5b000
David Celis <me@davidcel.is>
2016-08-02 13:40:07 -0700

    Use sha256
    Signed-off-by: David Celis <me@davidcel.is>

--- spark.rb
+++ spark.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Spark < Formula
   homepage 'http://zachholman.com/spark/'
   url 'https://github.com/davidcelis/spark/archive/v1.0.2.tar.gz'
-  sha1 '56dcb7aed0bba507e48513ec93db6204d4fd5e3f'
+  sha256 '9128f44487005a81d46d525bdb71cd1f7d457a7b298cc90cf03a4cce7ae4d762'
   version '1.0.2'
 
   def install
